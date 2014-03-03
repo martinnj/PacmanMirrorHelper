46148b0b8a914df6abb6a16c3e5217c94e2e4969
Martin J <martinnjorgensen@gmail.com>
2014-03-03 22:12:57 +0100

    Changed flow
    Takes backup of the old mirrorlist before it gets a new one.
    Gets list with all mirrors and ranks them, then chooses 6 fastest
    and puts them into a new list and removes the temporary. Solved #1

--- pacmrr.sh
+++ pacmrr.sh
@@ -5,17 +5,20 @@ if [[ $EUID -ne 0 ]]; then
     exit 1
 fi
 
-echo 'Fetching New mirrorlist from archlinux.org.'
-curl https://www.archlinux.org/mirrorlist/all/ -o /etc/pacman.d/mirrorlist
+echo 'Backing up current mirrorlist.'
+mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.old
 
-echo 'Backing up mirrorlist.'
-cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
+echo 'Fetching New mirrorlist from archlinux.org.'
+curl https://www.archlinux.org/mirrorlist/all/ -o /etc/pacman.d/mirrorlist.new
 
 echo 'Enabling all mirrors.'
-sed '/^#\S/ s|#||' -i /etc/pacman.d/mirrorlist.bak
+sed '/^#\S/ s|#||' -i /etc/pacman.d/mirrorlist.new
 
 echo 'Ranking enabled mirrors and selecting the 6 fastes. Will take time!'
-rankmirrors -n 6 /etc/pacman.d/mirrorlist.bak > /etc/pacman.d/mirrorlist
+rankmirrors -n 6 /etc/pacman.d/mirrorlist.new > /etc/pacman.d/mirrorlist
+
+echo 'Removing temporary mirrorlist.'
+rm /etc/pacman.d/mirrorlist.new
 
 echo 'Done. Please check /etc/pacman.d/mirrorlist for eventual errors.'
 exit 0
